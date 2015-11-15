5543dda9872183fb09e62a4162c376fbf000dec0
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2015-11-14 21:55:12 -0800

    change string comparison for env variables from is not to !=

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.mongoengine import MongoEngine
 import os
 app = Flask(__name__, instance_relative_config=True)
 print(os.environ)
-if os.environ.get("ENV") is not "heroku":
+if os.environ.get("ENV") != "heroku":
   app.config.from_pyfile('config.py')
 else:
   app.config.from_object(os.environ)
