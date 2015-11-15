e94e4dec9635cb617a07e5a0fe1445ebb8e2c67c
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2015-11-14 22:56:47 -0800

    add logs

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 import os
 app = Flask(__name__, instance_relative_config=True)
-print(os.environ)
 if os.environ.get("ENV") != "heroku":
   app.config.from_pyfile('config.py')
 else:
   app.config.from_object(os.environ)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 db = MongoEngine(app)
 from app import routes
