d9fa42f28a1ec2ac8b8a12e7be32a66c3f9686a2
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2015-11-15 03:33:34 -0800

    change the way heroku config is set

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,13 @@ app = Flask(__name__, instance_relative_config=True)
 if os.environ.get("ENV") != "heroku":
   app.config.from_pyfile('config.py')
 else:
-  app.config.from_object(os.environ.copy())
+  # Heroku environment variables are in os.environ, which can be converted into a dict.
+  # Iterate through this dict to create app config.
+  heroku_config = os.environ.copy()
+  for k in heroku_config:
+    if k.isupper():
+      app.config[k] = heroku_config[k]
+  print(app.config)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 db = MongoEngine(app)
