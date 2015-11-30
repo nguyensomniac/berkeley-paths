b68154947e9e51f3bac77e5a515253255eabf5de
Lily Nguyen <lanchi.v.nguyen@gmail.com>
2015-11-30 04:34:45 -0800

    add text and images to front page

--- server.py
+++ server.py
@@ -4,13 +4,12 @@ import os
 from flask.ext import assets
 
 env = assets.Environment(app)
+
 # Tell flask-assets where to look for our coffeescript and sass files.
 env.load_path = [
     os.path.join(os.path.dirname(__file__), 'bower_components'),
-    os.path.join(os.path.dirname(__file__), 'app', 'src', 'js'),
-    os.path.join(os.path.dirname(__file__), 'app', 'src', 'css')
+    os.path.join(os.path.dirname(__file__), 'app', 'src')
 ]
-
 
 env.register(
     'js_vendor',
@@ -26,23 +25,21 @@ env.register(
 env.register(
     'js_app',
     assets.Bundle(
-        'survey.js',
+        'js/*.js',
         filters='jsmin',
         output='app.js'
     )
 )
-
 env.register(
     'css_all',
     assets.Bundle(
+        'skeleton/css/normalize.css',
         'mapbox.js/mapbox.css',
         'skeleton/css/skeleton.css',
-        'webfonts.css',
-        'style.css',
+        'css/*.css',
         filters='cssmin',
         output='all.css'
     )
 )
-
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
